224b1dea63fbed5ac84a5f0b3d40b8ac3a8a45f7
KhoaHM <khoahm.it@gmail.com>
2023-09-29 13:33:13 +0700

    add The AuraWalletCoreConfigService class, which is responsible for managing configuration options for the Aura Wallet Core SDK. It provides essential configuration elements and initializes the SDK based on the selected environment.

---
+++ lib/src/cores/data_services/aura_wallet_core_config_service.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:aura_wallet_core/config_options/enviroment_options.dart';
+import 'package:alan/alan.dart';
+
+/// The [AuraWalletCoreConfigService] class provides configuration settings for the Aura Wallet Core SDK.
+class AuraWalletCoreConfigService {
+  const AuraWalletCoreConfigService();
+
+  // Access environment variables using Flutter Dotenv
+  Map<String, String> get env => dotenv.env;
+
+  // Decode the CHAIN_INFO environment variable into a map
+  Map<String, dynamic> get _chainInfo => jsonDecode(
+        env['CHAIN_INFO']!,
+      );
+
+  // Decode the NET_WORK_INFO environment variable into a map
+  Map<String, dynamic> get _netWorkInfo => jsonDecode(
+        env['NET_WORK_INFO']!,
+      );
+
+  // Access the 'coin' information from the chain configuration
+  Map<String, dynamic> get _coin => _chainInfo['coin'];
+
+  /// Gets the base URL for API requests.
+  String get baseUrl => _netWorkInfo['baseUrl'];
+
+  /// Gets the Bech32 configuration for the chain.
+  String get becH32Config => _netWorkInfo['bech32Config'];
+
+  /// Gets the chain ID.
+  String get chainId => _netWorkInfo['chainId'];
+
+  /// Gets the denomination of the coin.
+  String get deNom => _coin['denom'];
+
+  /// Gets the decimal precision of the coin.
+  int get decimal => _coin['decimal'];
+
+  /// Gets the gRPC host for network communication.
+  String get grpcHost => _netWorkInfo['grpcHost'];
+
+  /// Initializes the configuration based on the provided environment.
+  ///
+  /// [environment]: The Aura environment (testNet, euphoria, mainNet).
+  Future<void> init(AuraEnvironment environment) async {
+    const String baseAssetUri = 'packages/aura_wallet_core/assets/';
+
+    switch (environment) {
+      case AuraEnvironment.testNet:
+        await dotenv.load(fileName: '$baseAssetUri.env.testnet');
+        break;
+      case AuraEnvironment.euphoria:
+        await dotenv.load(fileName: '$baseAssetUri.env.euphoria');
+        break;
+      case AuraEnvironment.mainNet:
+        await dotenv.load(fileName: '$baseAssetUri.env.mainnet');
+        break;
+    }
+  }
+
+  /// Gets the LCD host for network communication.
+  String get lcdHost => _netWorkInfo['lcdHost'];
+
+  /// Gets network information including Bech32 configuration, LCD and gRPC hosts, and gRPC port.
+  NetworkInfo get networkInfo => NetworkInfo(
+        bech32Hrp: becH32Config,
+        lcdInfo: LCDInfo(host: lcdHost),
+        grpcInfo: GRPCInfo(
+          host: grpcHost,
+          port: grpcPort,
+        ),
+      );
+
+  /// Gets the gRPC port for network communication.
+  int get grpcPort => int.parse(_netWorkInfo['grpcPort']);
+
+  /// Generates a verification transaction URL based on the provided transaction hash (txHash).
+  String verifyTransactionUrl(String txHash) {
+    return '$baseUrl/api/v1/transaction?txHash=$txHash&chainid=$chainId';
+  }
+}
--- lib/src/cores/repo/store_house.dart
+++ lib/src/cores/repo/store_house.dart
@@ -1,22 +1,19 @@
-import 'package:alan/alan.dart';
 import 'package:aura_wallet_core/config_options/config_options.dart';
 import 'package:aura_wallet_core/config_options/enviroment_options.dart';
 import 'package:aura_wallet_core/src/cores/aura_internal_storage.dart';
+import 'package:aura_wallet_core/src/cores/data_services/aura_wallet_core_config_service.dart';
 
 /// The `Storehouse` class serves as a central hub for managing dependencies and
 /// providing access to essential configuration elements within the Aura Wallet Core.
 class Storehouse {
   Storehouse._(); // Private constructor prevents direct instantiation.
 
-  // Stores the current environment configuration for the Aura Wallet Core.
-  static late AuraEnvironment environment;
-
-  // Stores network information necessary for communication with the Aura Network.
-  static late NetworkInfo networkInfo;
-
   // Stores the instance of `AuraInternalStorage` for data storage and retrieval.
   static late AuraInternalStorage storage;
 
   // Stores the configuration options for the Aura Wallet Core.
   static late ConfigOption configOption;
+
+  // Config service contains static information.
+  static late AuraWalletCoreConfigService configService;
 }
