8ae6ade3b42a3cdf7045f7dd5a011d865875836a
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2023-10-03 10:02:35 +0700

    #Remove gaslimit ,minfee options

--- lib/config_options/config_options.dart
+++ lib/config_options/config_options.dart
@@ -1,11 +1,7 @@
 class ConfigOption {
   final bool isEnableLog;
-  final int gasLimit;
-  final int minFee;
 
   const ConfigOption({
     this.isEnableLog = false,
-    required this.gasLimit,
-    required this.minFee,
   });
 }
